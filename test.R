ce88a5a8231414ea4845216a4a8da6f5d8b2b886
yanxianUCSB <yanxian@umail.ucsb.edu>
2017-08-06 11:27:46 -0700

    append

--- test.R
+++ test.R
@@ -19,33 +19,7 @@ test.binodal.curve.jacobian_()
 
 
 
-# TEST 
-phi.polymer <- seq(0.001, 0.4, 0.001)
-phi.salt <- 0.000155
-temp <- 300
-chipw <- -0.9
-Chi <- matrix(c(.0,.0,0,0,chipw,
-               .0,.0,0,0,0,
-               0,0,0,0,0,
-               0,0,0,0,0,
-               chipw,0,0,0,0), 5,5)
 
-
-sysprop <- system.properties
-
-# TEST gibbs and gibbs dirivative
-ds <- gibbs.funs(phi.polymer, 
-            phi.salt = phi.salt,
-            temp = temp,
-            Chi = Chi,
-            alpha = get.alpha(temp, k.water.size),
-            sigma = sysprop$sigma,
-            polymer.num = sysprop$polymer.num,
-            size.ratio = sysprop$size.ratio,
-            molar.ratio = sysprop$molar.ratio)
-print(ds)
-plot(ds[-2])
-head(ds)
 stop()
 ds2 <- free.energy.funs(
     phi.polymer[1], 
