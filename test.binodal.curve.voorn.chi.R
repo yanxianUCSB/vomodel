7da32b6c37d0cf98638f7791e69045708f4f1085
yanxianUCSB <yanxian@umail.ucsb.edu>
2017-08-07 09:09:48 -0700

    updated

--- test.binodal.curve.voorn.chi.R
+++ test.binodal.curve.voorn.chi.R
@@ -12,45 +12,45 @@ library(nleqslv)
 DEBUG <- T
 SAVE <- F
 
-fitting.para$sampling.end <- 1e-4
+fitting.para$sampling.end <- 1e-3
 function(){
 }
 test.chi.chipw <- function(DEBUG = T, SAVE = F) {
+        
+    fitting.para$binodal.guess <- c(0.05, 0.03)
+    # fitting.para$sampling.start <- 0.0007875
     
-fitting.para$binodal.guess <- c(0.05, 0.03)
-# fitting.para$sampling.start <- 0.0007875
-
-ds <- NULL
-# Chi protein and water
-for (chipw in c( 0, 0.001)) {
-system.properties$Chi <- matrix(c(.0,.0,0,0, chipw,
-                                .0,.0,0,0,0,
-                                0,0,0,0,0,
-                                0,0,0,0,0,
-                                chipw,0,0,0,0), 5,5)
-
-p20 <- get.binodal.curve(80, system.properties$Chi, system.properties, fitting.para) %>% 
-    mutate(Chi = chipw)
+    ds <- NULL
+    # Chi protein and water
+    for (chipw in c( -0.01, 0, 0.01)) {
+        system.properties$Chi <- matrix(c(.0,.0,0,0, chipw,
+                                        .0,.0,0,0,0,
+                                        0,0,0,0,0,
+                                        0,0,0,0,0,
+                                        chipw,0,0,0,0), 5,5)
+        
+        p20 <- get.binodal.curve(80, system.properties$Chi, system.properties, fitting.para) %>% 
+            mutate(Chi = chipw)
+            
+        ds <- rbind(ds, p20)
+    }
     
-ds <- rbind(ds, p20)
-}
-
-# critical points
-print(unique(ds$critic.salt))
-# Plots
-g2 <- ggplot(ds, aes(y = round(phi.salt*100, digits = 4))) +
-    geom_point(aes(x = phi.polymer*100, col = as.factor(Chi)), lwd = 2) +
-    # geom_line(aes(x = phi.polymer*100, group = pairing, col = as.factor(Chi))) +
-    labs(x = 'Polymer [%]',
-         y = 'Salt [%]',
-         col = 'Chi pw')
-g2 <- theme.title.text.1(g2)
-
-
-print(g2)
-if(SAVE) {
-    ggsave('test.chi.chipw.png', g2, width = 6, height = 3)
-} 
+    # critical points
+    print(unique(ds$critic.salt))
+    # Plots
+    g2 <- ggplot(ds, aes(y = round(phi.salt*100, digits = 4))) +
+        geom_point(aes(x = phi.polymer*100, col = as.factor(Chi)), lwd = 2) +
+        # geom_line(aes(x = phi.polymer*100, group = pairing, col = as.factor(Chi))) +
+        labs(x = 'Polymer [%]',
+             y = 'Salt [%]',
+             col = 'Chi pw')
+    g2 <- theme.title.text.1(g2)
+    
+    
+    print(g2)
+    if(SAVE) {
+        ggsave('test.chi.chipw.png', g2, width = 6, height = 3)
+    } 
 }
 test.chi.chipw(DEBUG, SAVE)
 test.chi.chipq <- function(DEBUG = T, SAVE = F) {
