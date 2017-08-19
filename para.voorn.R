2e57752443e0ddcd40e29ddfd5970c24a688bef4
yanxianUCSB <yanxian@umail.ucsb.edu>
2017-08-19 16:40:30 -0700

    adapted para.voorn

--- para.voorn.R
+++ para.voorn.R
@@ -1,6 +1,6 @@
 system.properties <- list(
     polymer.num   = c(1000, 1000,   1, 1, 1),
-    sigma         = c(0.15, 0.15,   1, 1, 0),
+    sigma         = c(1, 1,   1, 1, 0),
     size.ratio    = rep(1, 5),
     MW            = c(1, 1),
     charge.ratio  = c(1, 1),
@@ -16,8 +16,11 @@ system.properties <- list(
 fitting.para <- list(
     epsilon = 1E-8 , 
     sampling.start = 1e-5,
-    sampling.gap = 1e-7 ,
-    critical.point.guess = c(phi.polymer = 0.01, phi.salt = 0.0001) ,
+    sampling.gap = 1e-5 ,
+    sampling.end = 5e-0,
+    critical.point.guess = c(phi.polymer = 0.01, phi.salt = 0.001) ,
     c.point.temp.fun = c.point.temp.fun(c.point.temp(system.properties, fitting.para)) ,
-    binodal.guess = 0.1  # phi.polymer.2
+    binodal.guess = 0.1,  # phi.polymer.2
+    condensation = F,
+    counterion.release = F
 )
