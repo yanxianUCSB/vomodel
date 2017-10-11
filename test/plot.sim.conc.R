4607642767f036ac21c3c1e132887db7912293e3
yanxianUCSB <yanxian@umail.ucsb.edu>
2017-10-11 14:56:51 -0700

    updated to include pdf output‘

--- test/plot.sim.conc.R
+++ test/plot.sim.conc.R
@@ -61,22 +61,34 @@ g <- ggplot(ds, aes(x = conc.polymer, y = conc.salt, group = tempC)) +
          col = 'Temp. [C]')
     
 
-g2 <- ggplot(ds2 %>% filter(conc.polymer < 20), aes(x = conc.polymer, y = tempC)) + 
-    geom_line(aes(col = 'sim'), lwd = 2) +
-    geom_point(data = phase.diagram.exp %>% filter(conc.salt == 0.03), aes(x = conc.polymer, y = tempC.cp, col = 'exp.cp')) +
+g2.transform <- function(x){-log(50-x)}
+g2 <- ggplot(ds2 %>% filter(conc.polymer < 20), aes(x = conc.polymer, y = g2.transform(tempC))) + 
+    geom_point(aes(pch = 'model'), size = 2) +
+    geom_point(data = phase.diagram.exp %>% filter(conc.salt == 0.03),
+               aes(x = conc.polymer, y = g2.transform(tempC.cp), pch = 'experiment')) +
     # geom_point(data = phase.diagram.exp %>% filter(conc.salt == 0.03), aes(x = conc.polymer, y = tempC.on, col = 'exp.on')) +
+    scale_y_continuous(limits = g2.transform(c(-160, 40)),
+                     breaks = g2.transform(seq(-160, 40, 40)), 
+                     labels = seq(-160, 40, 40)) +
     labs(x = 'Conc.polymer[mg/mL]', 
          y = 'Temperature [C]', 
-         col = '') 
+         pch = '') 
 
-g3 <- ggplot(ds3, aes(x = conc.salt, y = tempC)) + 
-    geom_line(aes(col = 'sim'), lwd = 2) +
-    geom_point(data = phase.diagram.exp %>% filter(abs(conc.polymer - 0.125) < 1e-3), aes(x = conc.salt, y = tempC.cp, col = 'exp.cp')) +
+g3.transform <- function(x){-log(50-x)}
+g3 <- ggplot(ds3, aes(x = conc.salt, y = g3.transform(tempC))) + 
+    geom_point(aes( pch = 'model'), size = 2) +
+    geom_point(data = phase.diagram.exp %>% filter(abs(conc.polymer - 0.125) < 1e-3),
+               aes(x = conc.salt, y = g3.transform(tempC.cp), pch = 'experiment'),
+               size = 2) +
+    scale_y_continuous(limits = g3.transform(c(-160, 40)),
+                     breaks = g3.transform(seq(-160, 40, 40)), 
+                     labels = seq(-160, 40, 40)) +
     # geom_point(data = phase.diagram.exp %>% filter(abs(conc.polymer - 0.125) < 1e-3), aes(x = conc.salt, y = tempC.on, col = 'exp.on')) +
     labs(x = 'NaCl [M]', 
          y = 'Temperature [C]', 
-         col = '')
-
+         pch = '') 
+ 
+g3 
 g <- theme.background.1(g)
 g <- theme.title.text.1(g)
 g2 <- theme.background.1(g2)
@@ -91,6 +103,9 @@ if(SAVE) {
     ggsave('plot.sim.conc.bncurve.png', g, width = 5, height = 5)
     ggsave( 'plot.sim.conc.conc.png', g2, width = 5, height = 5)
     ggsave( 'plot.sim.conc.nacl.png', g3, width = 5, height = 5)
+    ggsave('plot.sim.conc.bncurve.pdf', g, width = 5, height = 5)
+    ggsave( 'plot.sim.conc.conc.pdf', g2, width = 5, height = 5)
+    ggsave( 'plot.sim.conc.nacl.pdf', g3, width = 5, height = 5)
     saveRDS(object = ds, file = 'plot.sim.conc.ds', ascii = T)
     saveRDS(object = ds2, file = 'plot.sim.conc.conc.ds', ascii = T)
     saveRDS(object = ds3, file = 'plot.sim.conc.nacl.ds', ascii = T)
