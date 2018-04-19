1e507a61856eb33da782dc93821f57558a5a490e
Yanxian Lin <yanxian@umail.ucsb.edu>
2018-04-19 11:50:14 -0700

    ammended

--- test.r
+++ test.r
@@ -1,8 +1,8 @@
 source('vomodel.r')
 
 # Parameters
-POLYNUM       <<- c(200, 200,   1, 1, 1)
-SIGMA         <<- c(1 / 1, 1,   1, 1, 0)  # charge per monomer
+POLYNUM       <<- c(1000, 1000,   1, 1, 1)
+SIGMA         <<- c(0.15, 0.15,   1, 1, 0)  # charge per monomer
 SIZE          <<- c(1, 1, 1, 1, 1) * 0.31E-9  # m; Voorn Overbeek 1967
 MW            <<- c(1, 1)
 MOLARRATIO    <<- c(1,  1, 0.5, 0.5, 0)  # the polycation:polyanion and cation:anion molar ratio
@@ -21,6 +21,58 @@ get_ref <- function(){
   ds %>% saveRDS('ref.rds')
   return(ds)
 }
+
+# get_chi
+get_chi   <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1) {
+  phi_1 <-
+    c(phi1, p2p1 * phi1, phi3, phi3, 1 - phi1 - p2p1 * phi1 - 2 * phi3)
+  d_entropy_1 <-
+    1 / N[1] * log(phi_1[1]) + 1 / N[1] + (1 / N[2] * log(phi_1[2]) + 1 / N[2]) * p2p1
+  d_enthalpy_1 <- 
+    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_1) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
+  under_1 <- 
+    phi_1[5] * (p2p1 + 1) + (phi_1[1] + phi_1[2]) * p2p1
+  
+  phi_2 <-
+    c(phi2, p2p1 * phi2, phi3, phi3, 1 - phi2 - p2p1 * phi2 - 2 * phi3)
+  d_entropy_2 <-
+    1 / N[1] * log(phi_2[1]) + 1 / N[1] + (1 / N[2] * log(phi_2[2]) + 1 / N[2]) * p2p1
+  d_enthalpy_2 <- 
+    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_2) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
+  under_2 <- 
+    phi_2[5] * (p2p1 + 1) + (phi_2[1] + phi_2[2]) * p2p1
+  
+  chi <-
+    ((d_entropy_1 - d_entropy_2) + 
+       (d_enthalpy_1 - d_enthalpy_2)) / (under_2 - under_1)
+}
+test.get_chi <- function(){
+  get_ref() %>%
+    mutate(group = (phi1 - phi1[which.max(phi3)])<=0) ->
+    ds
+  dsref <- data.frame(
+    phi1 = ds$phi1[ds$group],
+    phi2 = rev(ds$phi1[!ds$group]),
+    phi3 = ds$phi3[!ds$group],
+    temp = ds$temp[ds$group]
+  )
+  apply(dsref %>% as.matrix(), 1, function(x){
+    get_chi(
+      phi1 = x[1],
+      phi2 = x[2],
+      phi3 = x[3],
+      temp = x[4],
+      N       = POLYNUM,
+      lattice = k.water.size,
+      sigma   = SIGMA,
+      p2p1    = p2p1
+    )
+  }) -> chi
+  return(
+    dsref %>% mutate(chi = chi)
+  )
+}
+
 fn        <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1){
   chi <- 0
   phi_1 <-
@@ -32,13 +84,12 @@ fn        <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1){
     gibbs(phi_2, temp, N, lattice, sigma, chi) +
     gibbs(phi_1, temp, N, lattice, sigma, chi)
 }
-
 sim_ <- function(x){
   x <- as.numeric(x)
   phi2 <- x[1]
   phi3 <- x[2]
   temp <- x[3]
-  initial_guesses <- c(0.01, 0.2)
+  initial_guesses <- c(0.01, 0.05)
   phi1 <- NULL
   tryCatch({
     root <- uniroot(
