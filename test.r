fb91c6031d04cf59acca6c62bafe2cfc936469a8
yanxianUCSB <yanxian@umail.ucsb.edu>
2018-04-24 22:52:11 -0700

    Worked as FH!

--- test.r
+++ test.r
@@ -1,14 +1,50 @@
+rm(list = ls())
 source('vomodel.r')
-
-# Parameters
-POLYNUM       <<- c(1000, 1000,   1, 1, 1)
-SIGMA         <<- c(0.15, 0.15,   1, 1, 0)  # charge per monomer
+library(dplyr)
+# Parameters --------------
+POLYNUM       <<- c(200, 3000,   1, 1, 1)
+SIGMA         <<- c(0, 0,   0, 0, 0)  # charge per monomer
 SIZE          <<- c(1, 1, 1, 1, 1) * 0.31E-9  # m; Voorn Overbeek 1967
-MW            <<- c(1, 1)
-MOLARRATIO    <<- c(1,  1, 0.5, 0.5, 0)  # the polycation:polyanion and cation:anion molar ratio
+# MW            <<- c(1, 1)
+MOLARRATIO    <<- c(3000,  10, 1, 1, 0)  # the polycation:polyanion and cation:anion molar ratio
 p2p1          <<- (POLYNUM[2] * MOLARRATIO[2] * SIZE[2]^3 ) / 
   (POLYNUM[1] * MOLARRATIO[1] * SIZE[1]^3 )  # the volume fraction ratio between specie 1 and specie 2
 
+# Functions ------------
+phi12phi <- function(phi1, phi3=5e-2){
+  phi <- c(phi1, phi1, phi3, phi3)
+  phi[5] <- 1 - sum(phi)
+  return(phi)
+}
+test.get.phi2.when.chi.is.0 <- function(phi1) {
+  temp <- 300
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
+  chi <- 0
+  x <- seq(1e-3, 0.4, 1e-2)
+  x %>% 
+    sapply(function(phi2){
+      get_chi(phi1, phi2, phi3 = 0.05, temp, N, lattice, sigma, p2p1 = 1) %>% 
+        return()
+    }) -> 
+    chis
+  y <- spline(x=chis, y=x, xout=0)$y
+  return(y)
+}  
+test.get.phi2.when.chi.is.0.with.config <- function(phi1, phi3, temp = 300, 
+                                          N=POLYNUM, lattice=0.31E-9, 
+                                          sigma=SIGMA, chi=0) {
+  x <- seq(1e-6, 0.2, 1e-4)
+  x %>% 
+    sapply(function(phi2){
+      get_chi(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1 = 1) %>% 
+        return()
+    }) -> 
+    chis
+  return(spline(x=chis, y=x, xout=chi)$y)
+}  
+# Get ref dataset ---------------
 get_ref <- function(){
   library(dplyr)
   path <- 'ref.chimad.txt'
@@ -21,143 +57,176 @@ get_ref <- function(){
   ds %>% saveRDS('ref.rds')
   return(ds)
 }
-
-# get_chi
-get_chi   <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1) {
-  phi_1 <-
-    c(phi1, p2p1 * phi1, phi3, phi3, 1 - phi1 - p2p1 * phi1 - 2 * phi3)
-  d_entropy_1 <-
-    1 / N[1] * log(phi_1[1]) + 1 / N[1] + (1 / N[2] * log(phi_1[2]) + 1 / N[2]) * p2p1
-  d_enthalpy_1 <- 
-    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_1) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
-  under_1 <- 
-    phi_1[5] * (p2p1 + 1) + (phi_1[1] + phi_1[2]) * p2p1
-  
-  phi_2 <-
-    c(phi2, p2p1 * phi2, phi3, phi3, 1 - phi2 - p2p1 * phi2 - 2 * phi3)
-  d_entropy_2 <-
-    1 / N[1] * log(phi_2[1]) + 1 / N[1] + (1 / N[2] * log(phi_2[2]) + 1 / N[2]) * p2p1
-  d_enthalpy_2 <- 
-    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_2) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
-  under_2 <- 
-    phi_2[5] * (p2p1 + 1) + (phi_2[1] + phi_2[2]) * p2p1
-  
-  chi <-
-    ((d_entropy_1 - d_entropy_2) + 
-       (d_enthalpy_1 - d_enthalpy_2)) / (under_2 - under_1)
-}
-test.get_chi <- function(){
-  get_ref() %>%
-    mutate(group = (phi1 - phi1[which.max(phi3)])<=0) ->
+get_ref_fh_chi_is_0 <- function(){
+  library(dplyr)
+  path <- 'ref.chimad.fh.chi.is.0.txt'
+  read.csv(path, sep = '\t') %>% 
+    mutate(phi1 = VolumeFraction / 2, phi3=Binodal / 2) %>% 
+    mutate(temp = 298.15) %>% 
+    arrange(phi1) %>% 
+    select(phi1, phi3, temp) ->
     ds
-  dsref <- data.frame(
-    phi1 = ds$phi1[ds$group],
-    phi2 = rev(ds$phi1[!ds$group]),
-    phi3 = ds$phi3[!ds$group],
-    temp = ds$temp[ds$group]
-  )
-  apply(dsref %>% as.matrix(), 1, function(x){
-    get_chi(
-      phi1 = x[1],
-      phi2 = x[2],
-      phi3 = x[3],
-      temp = x[4],
-      N       = POLYNUM,
-      lattice = k.water.size,
-      sigma   = SIGMA,
-      p2p1    = p2p1
-    )
-  }) -> chi
-  return(
-    dsref %>% mutate(chi = chi)
-  )
+  ds %>% saveRDS('ref.rds')
+  return(ds)
 }
-
-fn        <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1){
+# Test cases -------------
+test.case.1 <- function() {
+  # test gibbs
+  sapply(seq(1e-3, 1, 1e-3), function(phi1){
+    # temp <- 300
+    # N <- POLYNUM
+    # lattice <- 0.31E-9
+    # sigma <- SIGMA
+    # chi <- 0
+    phi <- c(phi1/2, phi1/2, 5e-2, 5e-2)
+    phi[5] <- 1 - sum(phi)
+    gibbs(phi = phi, temp = 303, N = POLYNUM, lattice = 0.31E-9, sigma = SIGMA, chi = 0) %>%
+      return()
+  }, simplify = T) %>% 
+    plot()
+  print('curve should be monotonic reducing')
+}  # test gibbs
+# test.case.1()
+test.case.3 <- function() {
+  temp <- 300
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
   chi <- 0
-  phi_1 <-
-    c(phi1, p2p1 * phi1, phi3, phi3, 1 - phi1 - p2p1 * phi1 - 2 * phi3)
-  phi_2 <-
-    c(phi2, p2p1 * phi2, phi3, phi3, 1 - phi2 - p2p1 * phi2 - 2 * phi3)
-  diff <- 
-    d_gibbs(phi_2, temp, N, lattice, sigma, chi, p2p1) * (phi2 - phi1) -
-    gibbs(phi_2, temp, N, lattice, sigma, chi) +
-    gibbs(phi_1, temp, N, lattice, sigma, chi)
+  seq(1e-3, 0.4, 1e-3) %>% 
+    sapply(function(phi1){
+      da1 <- numDeriv::grad(func = function(phi1){
+        phi <- c(phi1, phi1, 5e-2, 5e-2)
+        phi[5] <- 1 - sum(phi)
+        enthalpy <- -1 * get_alpha(temp, lattice) * sum(sigma * phi) ^ 1.5
+      }, x=phi1)
+      phi <- c(phi1, phi1, 5e-2, 5e-2)
+      phi[5] <- 1 - sum(phi)
+      da2 <- -1 * 3/2 * get_alpha(temp, lattice) * (sum(sigma * phi) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
+      da1 - da2
+    }) %>% 
+    plot
 }
-sim_ <- function(x){
-  x <- as.numeric(x)
-  phi2 <- x[1]
-  phi3 <- x[2]
-  temp <- x[3]
-  initial_guesses <- c(0.01, 0.05)
-  phi1 <- NULL
-  tryCatch({
-    root <- uniroot(
-      f = fn,
-      interval = initial_guesses,
-      phi2 = phi2,
-      phi3 = phi3,
-      temp = temp,
-      N       = POLYNUM,
-      lattice = k.water.size,
-      sigma   = SIGMA,
-      p2p1    = p2p1
-    )
-    phi1 <- root$root
-  }, error=function(e){
-    phi1 <- NA
-  }, warnings=function(w){
-  }, finally = {
-    phi1 <- ifelse(is.null(phi1), NA, phi1)
-  })
-  return(phi1)
+# test.case.3()  # d_enthalpy passes
+test.case.4 <- function() {
+  temp <- 300
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
+  chi <- 0
+  seq(1e-3, 0.1, 1e-3) %>% 
+    sapply(function(phi1){
+      da1 <- numDeriv::grad(x=phi1, func = function(phi1){
+        phi <- phi12phi(phi1)
+        entropy <- sum(phi / N * log(phi))
+      })
+      
+      phi <- phi12phi(phi1)
+      
+      da2 <-     
+        (1 / N[1] * log(phi[1]) + 1 / N[1]) + 
+        (1 / N[2] * log(phi[2]) + 1 / N[2]) * p2p1 +
+        (1 / N[5] * log(phi[5]) + 1 / N[5]) * (-(1 + p2p1))
+      
+      da1 - da2
+    }) %>% 
+    plot
 }
-# Thu Apr 19 10:15:02 2018  Test ---------------------- 
-test <- function(){
-  library(ggplot2)
-  # example <- as.matrix(get_expt())[2, 1:3]
-  example <- as.numeric(as.matrix(get_ref())[2, 1:3])
-  print(example)
-  phi2 <- example[1]
-  phi3 <- example[2]
-  temp <- example[3]
-  initial_guesses <- c(0.005, 0.01)
-  x <- seq(initial_guesses[1], initial_guesses[2], 1e-3)
-  y <- sapply(x, function(x){
-    fn(
-      phi1 = x,
-      phi2 = phi2,
-      phi3 = phi3,
-      temp = temp,
-      N       = POLYNUM,
-      lattice = k.water.size,
-      sigma   = SIGMA,
-      p2p1    = p2p1
-    )
-  }, simplify = T)
-  
-  ggplot(data.frame(x,y)) +
-    geom_line(aes(x,y))+
-    geom_hline(yintercept=0)+
-    scale_x_log10()
+# test.case.4()  # d_entropy passes
+test.case.2 <- function() {
+  sapply(seq(1e-3, 0.4, 1e-3), function(phi1){
+    phi <- c(phi1, phi1, 5e-2, 5e-2)
+    phi[5] <- 1 - sum(phi)
+    g <- d_gibbs(phi = phi, temp = 303, N = POLYNUM, lattice = 0.31E-9, 
+                 sigma = SIGMA, chi = 0) 
+    g2 <- numDeriv::grad(func = function(x){
+      phi <- c(x, x, 5e-2, 5e-2)
+      phi[5] <- 1 - sum(phi)
+      gibbs(phi = phi, temp = 303, N = POLYNUM, lattice = 0.31E-9, sigma = SIGMA, chi = 0)
+    }, x = phi1)
+    return(g-g2)
+  }, simplify = T) %>% 
+    plot()
 }
-test2 <- function(){
-  ref <- get_ref()
-  phi_dense <- apply(ref, 1, function(x) sim_(x))
-  if(F){
-    print('No fit found!')
-  } else {
-    ref %>% 
-      mutate(phi2 = phi_dense) ->
-      ds
-    ds %>% 
-      write.csv('simulated.csv', row.names = F)
-    return(ds)
-  }
+# test.case.2()  # d_gibbs passes
+test.case.5 <- function() {
+  temp <- 300
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
+  chi <- 0
+  seq(1e-6, 0.4, 1e-3) %>% 
+    sapply(function(phi1){
+      
+      phi2 <- .2
+      phi3 <- 0
+      get_chi(phi1, phi2, phi3 = phi3, temp, N, lattice, sigma, p2p1 = 1) %>% 
+        return()
+      
+    }) %>% 
+    plot(x=seq(1e-3, 0.4, 1e-3), ylab = 'chi', xlab = 'phi1')
+}  # get_chi
+# test.case.5()  # one case
+test.case.6 <- function() {
+  temp <- 300
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
+  chi <- 0
+  phi1s <- seq(1e-6, 0.4, 1e-3)
+  phi1s %>% 
+    sapply(function(phi1){
+      test.get.phi2.when.chi.is.0(phi1)
+    }) ->
+    phi2s
+  plot(phi1s, phi2s)
+}  # get phi2
+# test.case.6()  # one binodal curve for chi = 0
+test.case.7 <- function() {
+  SIGMA         <<- c(0, 0,   0, 0, 0)  # No VO term
+  ref <- get_ref_fh_chi_is_0()
+  1:nrow(ref) %>% 
+    sapply(function(row){
+      phi1 <- ref$phi1[row]
+      # phi3 <- ref$phi3[row]
+      phi3 <- 0
+      temp <- ref$temp[row]
+      test.get.phi2.when.chi.is.0.with.config(phi1, phi3, temp, 
+                                              N=POLYNUM, lattice=0.31E-9, 
+                                              sigma=SIGMA, chi=0) %>% 
+        return()
+    }) ->
+    phi2
+  ref$phi2 <- phi2
+  plot(c(ref$phi1, ref$phi2), c(ref$phi3, ref$phi3))
+  return(ref)
+}  # get_chi
+# ref <- test.case.7()  # one binodal curve for chi = 0
+# ref
+# Test fn -------------------
+# phi3 = 0.01
+# temp = 300
+# sigma = 0 (FH, no VO)
+# phi1, phi2 =  0.00155, 0.050
+#               0.00455, 0.035
+# phi3 = 0.0004504
+# phi1, phi2 =  7.458e-05, 0.32
+# 
+test.fn <- function() {
+  phi1 <- 7.458e-05
+  phi3 <- 0.0004504
+  temp <- 288
+  N <- POLYNUM
+  lattice <- 0.31E-9
+  sigma <- SIGMA
+  p2p1 <- p2p1
+  phi2s <- seq(1e-5, 0.4, 1e-3)
+  phi2s %>% 
+    sapply(function(phi2){
+      fn(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1)
+    }, simplify = T) ->
+    fn
+  plot(phi2s, fn)
 }
-# simulate()
-testds <- test2() %>% select(-temp)
-print(testds)
-plot(testds)
+test.fn()
 
-# plot(get_ref())
--- vomodel.R
+++ vomodel.R
@@ -1,4 +1,4 @@
-# Constant
+# Constant --------------
 kNa                         <<- 6.02E23     # Avogadro Number
 kkB                         <<- 1.38E-23    # Boltzmann Constant
 ke                          <<- 1.60E-19    # Elementary charge
@@ -16,52 +16,56 @@ k.water.conc                <<- 1000 / 18.01528 * 1000  # water concentration
 # p2p1          <<- (POLYNUM[2] * MOLARRATIO[2] * SIZE[2]^3 ) / 
 #   (POLYNUM[1] * MOLARRATIO[1] * SIZE[1]^3 )  # the volume fraction ratio between specie 1 and specie 2
 
-# system definition -------------
+# System definition -------------
 #' System is defined as phi, temp, N, lattice, sigma, chi
-get_alpha <- function(temp, lattice) {
+get_alpha  <- function(temp, lattice) {
   2 / 3 * sqrt(pi) * ((ke ^ 2 / (kEr * kkB * temp)) / lattice) ^ (3 / 2)
-}
-gibbs     <- function(phi, temp, N, lattice, sigma, chi) {
+}  
+gibbs      <- function(phi, temp, N, lattice, sigma, chi) {
   entropy <- sum(phi / N * log(phi))
   enthalpy <- -1 * get_alpha(temp, lattice) * sum(sigma * phi) ^ 1.5
   enthalpy_chi <- chi * (phi[1] + phi[2]) * phi[5]
   gibbs <- entropy + enthalpy + enthalpy_chi
 }
-d_gibbs   <- function(phi, temp, N, lattice, sigma, chi) {
+d_entropy  <- function(phi, N) {
   p2p1 <- phi[2] / phi[1]
-  d_entropy <-
-    (1 / N[1] * log(phi[1]) + 1 / N[1]) + 
-    (1 / N[2] * log(phi[2]) + 1 / N[2]) * p2p1 +
-    (1 / N[5] * log(phi[5]) + 1 / N[5]) * (-(1 + p2p1))
-  d_enthalpy <- 
-    -1 * 3/2 * get_alpha(temp, lattice) * (sum(sigma * phi) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
-  d_enthalpy_chi <- 
-    chi*phi[5] * (p2p1 + 1) + chi * (phi[1] + phi[2]) * p2p1
-  d_gibbs <- d_entropy + d_enthalpy + d_enthalpy_chi
+  (1 / N[1] * log(phi[1]) + 1 / N[1]) + 
+  (1 / N[2] * log(phi[2]) + 1 / N[2]) * p2p1 +
+  (1 / N[5] * log(phi[5]) + 1 / N[5]) * (-(1 + p2p1))
+}  # passed
+d_enthalpy <- function(phi, temp, lattice, sigma) {
+  p2p1 <- phi[2]/phi[1]
+  -1 * 3/2 * get_alpha(temp, lattice) * (sum(sigma * phi) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
+}  # passed
+d_enthalpy_chi <- function(phi, chi){
+  p2p1 <- phi[2]/phi[1]
+  chi * (phi[5]*(1 + p2p1) - phi[1]*(1+p2p1)^2)
 }
+d_gibbs   <- function(phi, temp, N, lattice, sigma, chi) {
+  p2p1           <- phi[2] / phi[1]
+  d_entropy      <- d_entropy(phi, N)
+  d_enthalpy     <- d_enthalpy(phi, temp, lattice, sigma)
+  d_enthalpy_chi <- d_enthalpy_chi(phi, chi)
+  d_gibbs        <- d_entropy + d_enthalpy + d_enthalpy_chi
+}  # passed
 get_chi   <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1) {
+  if(phi1 == phi2) stop(msg = 'phi == phi2 for get_chi!')
   phi_1 <-
     c(phi1, p2p1 * phi1, phi3, phi3, 1 - phi1 - p2p1 * phi1 - 2 * phi3)
-  d_entropy_1 <-
-    1 / N[1] * log(phi_1[1]) + 1 / N[1] + (1 / N[2] * log(phi_1[2]) + 1 / N[2]) * p2p1
-  d_enthalpy_1 <- 
-    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_1) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
-  under_1 <- 
-    phi_1[5] * (p2p1 + 1) + (phi_1[1] + phi_1[2]) * p2p1
-  
   phi_2 <-
     c(phi2, p2p1 * phi2, phi3, phi3, 1 - phi2 - p2p1 * phi2 - 2 * phi3)
-  d_entropy_2 <-
-    1 / N[1] * log(phi_2[1]) + 1 / N[1] + (1 / N[2] * log(phi_2[2]) + 1 / N[2]) * p2p1
-  d_enthalpy_2 <- 
-    3/2 * get_alpha(temp, lattice) * (sum(sigma * phi_2) ^ 0.5) * (sigma[1] + sigma[2] * p2p1)
-  under_2 <- 
-    phi_2[5] * (p2p1 + 1) + (phi_2[1] + phi_2[2]) * p2p1
-  
+  d_entropy_1 <- d_entropy(phi_1, N)
+  d_entropy_2 <- d_entropy(phi_2, N)
+  d_enthalpy_1 <- d_enthalpy(phi_1, temp, lattice, sigma)
+  d_enthalpy_2 <- d_enthalpy(phi_2, temp, lattice, sigma)
+  under_1 <- (phi_1[5]*(1 + p2p1) - phi_1[1]*(1+p2p1)^2)
+  under_2 <- (phi_2[5]*(1 + p2p1) - phi_2[1]*(1+p2p1)^2)
   chi <-
-    ((d_entropy_1 - d_entropy_2) + 
-       (d_enthalpy_1 - d_enthalpy_2)) / (under_2 - under_1)
-}
+    ((d_entropy_1 - d_entropy_2) + (d_enthalpy_1 - d_enthalpy_2)) / 
+    (under_2 - under_1)
+  return(chi)
+}  # should be good
+# Fn --------------------
 #' fn = fn(x), here x=phi1, other parameters are known
 fn        <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1){
   chi <- get_chi(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1)
@@ -70,12 +74,12 @@ fn        <- function(phi1, phi2, phi3, temp, N, lattice, sigma, p2p1){
   phi_2 <-
     c(phi2, p2p1 * phi2, phi3, phi3, 1 - phi2 - p2p1 * phi2 - 2 * phi3)
   diff <- 
-    d_gibbs(phi_2, temp, N, lattice, sigma, chi, p2p1) * (phi2 - phi1) -
+    d_gibbs(phi_2, temp, N, lattice, sigma, chi) * (phi2 - phi1) -
     gibbs(phi_2, temp, N, lattice, sigma, chi) +
     gibbs(phi_1, temp, N, lattice, sigma, chi)
 }
 
-# experiment data process ---------------
+# Data preparation ---------------
 # this part of code transform the experimental observations into volume fraction
 # and temperature in Kelvin
 get_expt  <- function() {
@@ -92,13 +96,13 @@ get_expt  <- function() {
     saveRDS('expt.rds')
   return(ds)
 }
-# this part runs the simulation ------------------
-sim_ <- function(x){
+# Simulate ------------------
+sim_      <- function(x){
   x <- as.numeric(x)
   phi2 <- x[1]
   phi3 <- x[2]
   temp <- x[3]
-  initial_guesses <- phi2 + (1-phi2) * c(0.0001, 0.5)
+  initial_guesses <- c(0.1, 0.5)
   phi1 <- NULL
   tryCatch({
     root <- uniroot(
@@ -121,7 +125,15 @@ sim_ <- function(x){
   })
   return(phi1)
 }
-simulate  <- function(){
+simulate  <- function(output.filename.no.ext = 'simulated'){
+  # get_chi(...) for dplyr::mutate
+  get_chi_ <- function(phi1, phi2, phi3, temp){
+    seq(1, length(phi1), 1) %>% 
+      sapply(function(i){
+        get_chi(phi1 = phi1[i], phi2 = phi2[i], phi3 = phi3[i], temp = temp[i], 
+                N = POLYNUM, lattice = k.water.size, sigma = SIGMA, p2p1 = p2p1)
+      })
+  }
   expt <- get_expt()
   phi_dense <- apply(as.matrix(expt), 1, sim_)
   if(is.null(phi_dense) | 
@@ -130,7 +142,23 @@ simulate  <- function(){
   } else {
     expt %>% 
       mutate(phi2 = phi_dense) %>% 
-      write.csv('simulated.csv', row.names = F)
+      mutate(chi = get_chi_(phi1, phi2, phi3, temp)) -> output
+    output %>% 
+      write.csv(paste0(output.filename.no.ext, '.csv'), row.names = F)
+    output %>% 
+      saveRDS(paste0(output.filename.no.ext, '.rds'))
   }
 }
-# simulate()
+# Modeling ------------
+simulate.fh <- function(){
+  POLYNUM       <<- c(207, 2939,   1, 1, 1)
+  SIGMA         <<- c(0, 0,   0, 0, 0)  # charge per monomer
+  SIZE          <<- c(1, 1, 1, 1, 1) * 0.31E-9  # m; Voorn Overbeek 1967
+  MW            <<- c(22e3, 900e3)
+  MOLARRATIO    <<- c(2939,  11, 0.5, 0.5, 0)  # the polycation:polyanion and cation:anion molar ratio
+  p2p1          <<- (POLYNUM[2] * MOLARRATIO[2] * SIZE[2]^3 ) /
+    (POLYNUM[1] * MOLARRATIO[1] * SIZE[1]^3 )  # the volume fraction ratio between specie 1 and specie 2
+  
+  simulate('FH')
+}
+simulate.fh()
