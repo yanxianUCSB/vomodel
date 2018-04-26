a8f38ac4735b4e466e8353ab12770c2f5861e213
Yanxian Lin <yanxian@umail.ucsb.edu>
2018-04-26 13:23:42 -0700

    plot only the dilute phase

--- plot.fitting.results.R
+++ plot.fitting.results.R
@@ -20,18 +20,16 @@ get.dataset <- function(path = 'results/'){
     ) 
 }
 # Plotting -------------
-plot <- function(){
-  dataset <- get.dataset()
+plot <- function(dataset){
   ggplot() +
-    geom_point(aes(x = phi1, y = temp, color = 'Expt', pch = phi3), 
+    geom_point(aes(x = phi1, y = temp, color = phi3), 
                data = dataset %>% filter(group == 'Expt')) +
-    geom_point(aes(x = phi2, y = temp, color = 'Expt', pch = phi3), 
-               data = dataset %>% filter(group == 'Expt')) + 
-    geom_line(aes(x = phi1, y = temp, color = 'Fit'),
+    # geom_point(aes(x = phi2, y = temp, color = phi3), 
+    #            data = dataset %>% filter(group == 'Expt')) + 
+    geom_line(aes(x = phi1, y = temp, color = phi3),
               data = dataset %>% filter(group == 'Fit')) +
-    geom_line(aes(x = phi2, y = temp, color = 'Fit'),
-              data = dataset %>% filter(group == 'Fit')) +
-    facet_wrap(~mod) +
+    # geom_line(aes(x = phi2, y = temp, color = phi3),
+    #           data = dataset %>% filter(group == 'Fit')) +
     scale_x_log10()+
     labs(
       x = expression(paste(phi["1"])),
@@ -42,5 +40,17 @@ plot <- function(){
     theme_bw()+
     ggsci::scale_color_npg()
 }
-plot()
-ggsave('results/fitting.png', dpi = 300, width = 5, height = 3)
+plot.mod <- function(){
+  plot(get.dataset()) +
+    facet_wrap(~mod) 
+  ggsave('results/fitting.png', dpi = 1000, width = 5, height = 3)
+  ggsave('results/fitting_wide.png', dpi = 1000, width = 8, height = 3)
+}
+plot.FHVO <- function(){
+  g <- plot(get.dataset() %>% 
+         filter(mod == 'FHVO'))
+  ggsave('results/fitting_fhvo.png', dpi = 1000, width = 5, height = 3)
+  saveRDS(g, 'results/fitting_fhvo.rds')
+}
+plot.mod()
+plot.FHVO()
